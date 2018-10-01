1265ea6dc4e1eb278e502eef2bf41d066a33aa14
Imanol <imanol.luis@ikasle.egibide.org>
2018-10-01 10:25:05 +0200

    Subpágina modo automatico

---
+++ Web/js/automatico.js
@@ -0,0 +1,19 @@
+function botonStart() {
+    alert("El autómata se ha iniciado.");
+    /*var automatico=:="WEB_1"."BOTON AUTOMATICO":;
+    if (!automatico)
+    {
+        automatico=true;
+        :=WEB_1".BOTON AUTOMATICO:=automatico;
+    }*/
+}
+
+function botonStop() {
+    alert("El autómata se ha parado.");
+    /*var automatico=:="WEB_1".BOTON AUTOMATICO:;
+    if (automatico)
+    {
+        automatico=false;
+        :=WEB_1".BOTON AUTOMATICO:=automatico;
+    }*/
+}
