d46f94cc171b07b9836d66e91e45a0ccdddb7272
Jon Maneiro <jon.maneiro@ikasle.egibide.org>
2018-10-05 12:55:38 +0200

    Funcionalidad de MM
    Adaptada a los ultimos cambios que nos han dicho que implementemos

--- Web/js/sub_mm.js
+++ Web/js/sub_mm.js
@@ -1,3 +1,6 @@
+/*Variables Globales*/
+  var mmCheck = false;
+/*Variables Globales*/
 function pruebaBoolean(element) {
     let boton = document.getElementById(element);
     boton.setAttribute('disabled', '');
@@ -13,7 +16,7 @@ function activarB() {
 }
 
 /*ESTA FUNCION ES MUY IMPORTANTE, VOY A TOMARLA COMO BASE PARA EL RESTO DE LA LOGICA DE ESTO*/
-function enviarDatos(nomVar,val){
+/*function enviarDatos(nomVar,val){
     var datos = '\"WEB_1\".'+nomVar+"="+val;
     $($.ajax({
         method:'POST',
@@ -25,11 +28,13 @@ function enviarDatos(nomVar,val){
             console.log("errores");
         }
     }))
-}
-
+}*/
+/*Envia los datos de las posiciones y de la velocidad*/
 function enviarDatos(){
-    var datos = '\"WEB_1\".'+"POSITION_1"+"="+document.getElementById("position").value+"&"+'\"WEB_1\".'+"SPEED_1"
-        +"="+document.getElementById("speed").value+"&"+'\"WEB_1\".'+"BOTON_MM"+"=true";
+    var datos = '\"WEB_1\".'+"POSITION_1"+"="+document.getElementById("position1").value+"&"+'\"WEB_1\".'+"POSITION_2"+"="+document.getElementById("position2").value
+    +"&"+'\"WEB_1\".'+"POSITION_3"+"="+document.getElementById("position3").value+"&"
+    +'\"WEB_1\".'+"POSITION_4"+"="+document.getElementById("position4").value+"&"+'\"WEB_1\".'+"SPEED_1"
+        +"="+document.getElementById("speed").value;
     alert(datos);
     $($.ajax({
         method:'POST',
@@ -41,4 +46,63 @@ function enviarDatos(){
             console.log("errores");
         }
     }))
+}
+/*Activa o desactiva el modo MM*/
+function alternarMM(){
+  if (mmCheck==false) {
+    mmCheck=true;
+    enviarDatos();
+    document.getElementById("submit").style.backgroundColor="#ffb84d";
+    document.getElementById("mm_empaquetar").disabled=false;
+    document.getElementById("mm_sellar").disabled=false;
+    modoActivo=true;
+  }
+  else{
+    mmCheck=false;
+    document.getElementById("submit").style.backgroundColor="#01313a";
+    document.getElementById("mm_empaquetar").disabled=true;
+    document.getElementById("mm_sellar").disabled=true;
+    modoActivo=false;
+  }
+  var datosx = '\"WEB_1\".BOTON_MM='+mmCheck;
+  $($.ajax({
+    method:'POST',
+    data:datosx,
+    success:function(){
+      console.log("funciona, los datos que se han enviado son: "+'\"WEB_1\".BOTON_MM='+mmCheck);
+    },
+    error:function(){
+      console.log("errores");
+    }
+  }))
+}
+/*Esta funcion envia los datos de las variables al pulsar los botones empaquetar y sellar*/
+function enviarSennalxx(nomVar,val){
+  var datos = '\"WEB_1\".'+nomVar+"="+val;
+  /*Enviar la señal inicial*/
+  $($.ajax({
+    method:'POST',
+    data:datos,
+    success:function(datos){
+      console.log("funciona, el dato que se ha enviado es "+'\"WEB_1\".'+nomVar+"="+val);
+    },
+    error: function(){
+      console.log("errores");
+    }
+  }))
+  /*Devolver false para hacer efecto de On/Off*/
+
+  var altern = '\"WEB_1\".'+nomVar+"="+!val;
+  setTimeout(function(){
+  $($.ajax({
+    method:'POST',
+    data:altern,
+    success:function(altern){
+      console.log("funciona, el valor enviado es "+'\"WEB_1\".'+nomVar+"="+!val);
+    },
+    error: function(){
+      console.log("errores");
+    }
+  }))
+},1000);
 }
